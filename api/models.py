e0cf5b3f4fccdc0d8bad2d0f21de0c5708de382f
rodolfo <rodolfogroero2@gmail.com>
2024-10-25 16:49:26 -0400

    feat:new relations added

--- api/models.py
+++ api/models.py
@@ -14,8 +14,19 @@ class Product(models.Model):
     stock = models.PositiveIntegerField(default=0)
     created_at = models.DateTimeField(default=timezone.now, blank=True)
 
+class Contact(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    number = models.CharField(max_length=15)
+    is_customer = models.BooleanField(default=False)
+    is_supplier = models.BooleanField(default=False)
+    debt = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    credit = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    created_at = models.DateTimeField(default=timezone.now, blank=True)
+
 class Sale(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    contact = models.ForeignKey(Contact, on_delete=models.SET_NULL, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     quantity = models.PositiveIntegerField()
     unit_price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -23,6 +34,7 @@ class Sale(models.Model):
 
 class Purchase(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    contact = models.ForeignKey(Contact, on_delete=models.SET_NULL, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     quantity = models.PositiveIntegerField()
     unit_price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -38,12 +50,4 @@ class Card(models.Model):
     name = models.CharField(max_length=100)
     number = models.CharField(max_length=16)
     balance = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(default=timezone.now, blank=True)
-
-class Contact(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    number = models.CharField(max_length=15)
-    is_customer = models.BooleanField(default=False)
-    is_supplier = models.BooleanField(default=False)
     created_at = models.DateTimeField(default=timezone.now, blank=True)
