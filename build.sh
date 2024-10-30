1a4bb33ee6cdf5f9cb8af64fd9db59deda4e55db
rodolfo <rodolfogroero2@gmail.com>
2024-10-30 07:48:46 -0400

    build.sh create superuser now

--- build.sh
+++ build.sh
@@ -6,3 +6,20 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py migrate
+
+# Crear superusuario
+python manage.py shell -c "
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('Rodolfo', 'rodolfogroero2@gmail.com', 'semedesempezcuecicrespalapuerca')
+"
