eb3007bc90be9f34a79658b090c2dc47b36d8dfc
rodolfo <rodolfogroero2@gmail.com>
2024-10-30 14:28:59 -0400

    connecting to pgsql database

--- mi_empresa_virtual/settings.py
+++ mi_empresa_virtual/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,12 +85,31 @@ WSGI_APPLICATION = 'mi_empresa_virtual.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'mi_empresa_virual_pgsql',
+            'USER': 'mi_empresa_virual_pgsql_user',
+            'PASSWORD': 'VvzZ01onJETMZTZQtUU7gtsihusF2H6D',
+            'HOST': 'dpg-cnvvvhf109ks73f2aqe0-a',  # Reemplaza con tu host de Render
+            'PORT': '5432',
+        }
+    }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
