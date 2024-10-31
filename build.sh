c3fe6ec56d06c6a026135826c044768b76e6b3dd
rodolfo <rodolfogroero2@gmail.com>
2024-10-31 12:57:19 -0400

    error with superuser fixed

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ python manage.py migrate
 python manage.py shell -c "
 from django.contrib.auth import get_user_model;
 User = get_user_model();
-if not User.objects.filter(username='rodolfo').exists():
+if not User.objects.filter(username='admin').exists():
     User.objects.create_superuser(
         'admin',
         'admin@gmail.com',
