13f01b71d1b0db33fe263d8203d758b70e5240aa
rodolfo <rodolfogroero2@gmail.com>
2024-11-05 15:20:57 -0500

    change setings and creating static folder

--- mi_empresa_virtual/settings.py
+++ mi_empresa_virtual/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = "/home/ecomcuba/public_html/static"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
